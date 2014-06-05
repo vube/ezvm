c402e4401ca2a87d659cd053cd57ee35fa53a209
Ross Perkins <ross@vubeology.com>
2014-06-05 12:44:59 -0700

    Improved selfupdate procedure to ensure ALL updates take effect before running update. Added user/environment debug output.

--- commands/update.sh
+++ commands/update.sh
@@ -8,7 +8,7 @@ WITH_SELF_UPDATE=0
 
 ORIGINAL_ARGS=$@
 
-while getopts ":d:qsV:" flag; do
+while getopts ":d:qsSV:" flag; do
     case "$flag" in
         d)
             EZVM_LOCAL_CONTENT_DIR=$OPTARG
@@ -18,6 +18,9 @@ while getopts ":d:qsV:" flag; do
             ;;
         s)
             WITH_SELF_UPDATE=1
+            ;;
+        S)
+            WITH_SELF_UPDATE=0
             ;;
         V)
             export EZVM_VERBOSITY=$OPTARG
@@ -33,6 +36,10 @@ $EZVM_BIN_DIR/verbose-log 2 <<END_LOG
 $0 update $ORIGINAL_ARGS
 Working on BASE=$EZVM_BASE_DIR
 
+User: `id`
+Environment:
+`env`
+
 END_LOG
 
 # If we are updating ezvm itself as well, do that first
@@ -42,9 +49,17 @@ if [ $WITH_SELF_UPDATE = 1 ]; then
 
     # run `ezvm selfupdate` in a sub-shell so it doesn't modify our variables
     ( . $EZVM_COMMANDS_DIR/selfupdate.sh ) || die "self update failed" $?
+
+    # Now start `ezvm update` in a new shell so that any/all updates to self
+    # take effect.
+    #
+    # Here we append the -S (NO self update) flag to the end, so that we effectively
+    # remove the -s flag which is buried somewhere in $ORIGINAL_ARGS
+
+    log_msg 10 "Re-running ezvm update with -S (no selfupdate) flag"
+    $EZVM_BASE_DIR/bin/ezvm update $ORIGINAL_ARGS -S || die "ezvm update failed: exit code=$?" $?
+else
+
+    # Not running selfupdate, so just run the update procedure
+    ( . $EZVM_COMMANDS_DIR/utilities/update-procedure.sh ) || die "update procedure failed" $?
 fi
-
-# Now it's quite possible that all the files we rely on have changed.
-# Maybe even the files we've already run and executed.
-
-( . $EZVM_COMMANDS_DIR/utilities/update-procedure.sh ) || die "update procedure failed" $?
