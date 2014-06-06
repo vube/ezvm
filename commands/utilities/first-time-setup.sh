f54e2c6a2fc4408807e362bab95c436b121cd73b
Ross Perkins <ross@vubeology.com>
2014-06-06 14:50:57 -0700

    Moved home dir copy procedure to its own lib

--- commands/utilities/first-time-setup.sh
+++ commands/utilities/first-time-setup.sh
@@ -5,11 +5,9 @@
 #
 
 $EZVM_BIN_DIR/verbose-log 2 <<END_LOG
-
 commands/utilities/first-time-setup.sh
 
 END_LOG
-
 
 if [ -e $FIRST_TIME_SETUP_FILE ]; then
     warn "First time setup has already been run on this machine; running anyway..."
@@ -22,20 +20,9 @@ if [ ! -d $FIRST_TIME_SETUP_DIR ]; then
     mkdir -p $FIRST_TIME_SETUP_DIR || die "Cannot create first time setup dir: $FIRST_TIME_SETUP_DIR" 12
 fi
 
-# If there is a home directory in the local content dir (etc/local by default)
-# then we want to copy it to the system.
-EZVM_HOME_SRC=${EZVM_HOME_SRC:-"$EZVM_LOCAL_CONTENT_DIR/home"}
-if [ -d $EZVM_HOME_SRC ]; then
-
-    # If there is a version specifically for the current user, use that version
-    # instead of the default version
-    if [ -d "$EZVM_HOME_SRC/users/$EZVM_USERNAME" ]; then
-        EZVM_HOME_SRC="$EZVM_HOME_SRC/users/$EZVM_USERNAME"
-    fi
-
-    log_msg 3 "cp -rf $EZVM_HOME_SRC $HOME"
-    cp -rf $EZVM_HOME_SRC $HOME || die "Failed to copy home directory files from $EZVM_HOME_SRC to $HOME" $?
-fi
+# Copy the user home dirs (if any) onto the machine
+. $EZVM_BASE_DIR/libs/copy-home-dir.sh
+copy_home_dir
 
 log_msg 1 "First time setup completed successfully"
 date > $FIRST_TIME_SETUP_FILE
---
+++ libs/copy-home-dir.sh
@@ -0,0 +1,53 @@
+# libs/copy_home_dir.sh
+#
+# Sub-routines to handle copying a user's homedir to the local machine.
+# You can make an update script that calls this if you want to allow
+# this to happen every time you `ezvm update`
+#
+
+
+# If there is a home directory in the local content dir (etc/local by default)
+# then we want to copy it to the system.
+EZVM_HOME_SRC=${EZVM_HOME_SRC:-"$EZVM_LOCAL_CONTENT_DIR/home"}
+
+
+# Copy home source to $HOME
+# Usage: copy_home_src $source_directory
+copy_home_dir_src() {
+
+    local src="$1"
+    local files=$(find "$src" -maxdepth 1 | sed -e "s,^$src/*,," | grep -v '^users$') || die "Error searching home source: $src" $?
+    local f
+
+    log_msg 7 "Copying home dir files from $src"
+
+    for f in $files; do
+        log_msg 9 "Overwrite: $HOME/$f"
+        cp -r "$src/$f" "$HOME/$f" || die "Error copying $f" $?
+    done
+}
+
+
+# Copy the default home directory from the local source to the current machine.
+# If there is also a specific user home directory, copy that too.
+# Usage: copy_home_dir
+copy_home_dir() {
+
+    if [ -d $EZVM_HOME_SRC ]; then
+
+        log_msg 5 "Copying generic home dir source"
+        copy_home_dir_src "$EZVM_HOME_SRC"
+
+        # If there is a version specifically for the current user, use that version
+        # in addition to the default version
+        if [ -d "$EZVM_HOME_SRC/users/$EZVM_USERNAME" ]; then
+
+            log_msg 5 "Copying user-specific home dir source"
+            copy_home_dir_src "$EZVM_HOME_SRC/users/$EZVM_USERNAME"
+        fi
+
+    else
+
+        log_msg 10 "Notice: No home dir source dir exists: $EZVM_HOME_SRC"
+    fi
+}
